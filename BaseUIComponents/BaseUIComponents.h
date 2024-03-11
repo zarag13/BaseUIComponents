daf6a9187bb61a2c4eb50d524377d27eafe3614d
zarag13 <Ksdobnikov@icloud.com>
2024-03-11 18:00:12 +0200

    Initial Commit

---
+++ BaseUIComponents/BaseUIComponents.h
@@ -0,0 +1,18 @@
+//
+//  BaseUIComponents.h
+//  BaseUIComponents
+//
+//  Created by Kirill on 11.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseUIComponents.
+FOUNDATION_EXPORT double BaseUIComponentsVersionNumber;
+
+//! Project version string for BaseUIComponents.
+FOUNDATION_EXPORT const unsigned char BaseUIComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseUIComponents/PublicHeader.h>
+
+
